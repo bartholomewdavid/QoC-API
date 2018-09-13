9a0175d7a58ba20e181c533bcd6dcd1fe458b952
David Bartholomew <bartholomew.david@gmail.com>
2018-09-12 20:48:28 -0400

    Small cleanup

--- qoc-api/app.js
+++ qoc-api/app.js
@@ -11,8 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS to appease the browser
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
