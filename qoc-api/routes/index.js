785bb9105aa58cc0bd03264d39be3d8550291429
David Bartholomew <bartholomew.david@gmail.com>
2018-09-13 11:15:12 -0400

    Forgot to switch back to public api

--- qoc-api/routes/index.js
+++ qoc-api/routes/index.js
@@ -5,12 +5,12 @@ var request = require('request');
 
 router.get('/list', function(req, res, next) {
   request.get({
-    url: 'https://webhook.site/8a7e3773-a3b9-44c2-b0e6-004284d877dd',
-    headers: {
-      'content-type': 'text/javascript; charset=UTF-8'
-    }
+    url: 'http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json',
+    encoding: 'utf-8',
+    gzip: true
   }, function (apiError, apiRes, apiBody) {
     // Errors are not accounted for
+
     var data = JSON.parse(apiBody);
     var entries = data.feed.entry;
 
@@ -22,10 +22,9 @@ router.get('/details/:appId', function(req, res, next) {
   const appId = req.params.appId;
 
   request.get({
-    url: 'https://webhook.site/8a7e3773-a3b9-44c2-b0e6-004284d877dd',
-    headers: {
-      'content-type': 'text/javascript; charset=UTF-8'
-    }
+    url: 'http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json',
+    encoding: 'utf-8',
+    gzip: true
   }, function (apiError, apiRes, apiBody) {
     // Errors are not accounted for
     var data = JSON.parse(apiBody);
